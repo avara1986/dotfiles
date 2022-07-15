60b6055d60b9257db123755f9cfdc38777589ac1
Alberto Vara <a.vara.1986@gmail.com>
2022-07-15 19:16:42 +0200

    Add init configuration script

---
+++ init_dotfiles.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+## COLORS
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+## CONSTANTS
+ZSH_DOT_FILE="$HOME/.zshrc"
+I3_CONFIG="$HOME/dotfiles/i3/config"
+
+if ! command -v zsh &> /dev/null
+then
+    echo -e "ZHS could not be found. Installing"
+    sudo apt-get install -y zsh curl git fonts-powerline
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    rm $ZSH_DOT_FILE
+else
+    echo -e "${GREEN} * ZSH IS INSTALLED${NC}"
+fi
+
+
+if [ -f "$ZSH_DOT_FILE" ]
+then
+    echo -e "${GREEN} * ${ZSH_DOT_FILE} exists ${NC}"
+else
+    ln -s $HOME/dotfiles/.zshrc $ZSH_DOT_FILE
+fi
+
+
+if ! command -v i3 &> /dev/null
+then
+    echo -e "I3 could not be found. Installing"
+    sudo apt-get install -y i3
+    rm $I3_CONFIG
+    # sudo apt-get install polybar rofi
+    # git submodule add https://github.com/nimishgo/i3wm-themes.git
+    # mkdir $HOME/.config/polybar
+    # git submodule init
+    # git submodule update
+    # cp $HOME/dotfiles/i3wm-themes/Subway/.i3/config $HOME/.config/i3/config
+    # cp $HOME/dotfiles/i3wm-themes/Subway/.config/polybar/* $HOME/.config/polybar/
+else
+    echo -e "${GREEN} * I3 IS INSTALLED${NC}"
+fi
+
+if [ -f "$I3_CONFIG" ]
+then
+    echo -e "${GREEN} * ${I3_CONFIG} exists ${NC}"
+else
+    ln -s $HOME/dotfiles/i3/config $I3_CONFIG
+fi
+
+if ! command -v pyenv &> /dev/null
+then
+    echo -e "PYENV could not be found. Installing"
+    sudo apt-get install -y zlib1g-dev make build-essential libssl-dev libffi-dev git libbz2-dev libsqlite3-dev libreadline-dev libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev
+    git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+    pyenv install 2.7:latest
+	pyenv install 3.6:latest
+else
+    echo -e "${GREEN} * PYENV IS INSTALLED${NC}"
+fi
+
+if ! command -v docker &> /dev/null
+then
+    echo -e "DOCKER could not be found. Installing"
+    sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+   sudo apt-get update
+   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+   sudo docker run hello-world
+   sudo groupadd docker
+   sudo usermod -aG docker $USER
+else
+    echo -e "${GREEN} * DOCKER IS INSTALLED${NC}"
+fi
